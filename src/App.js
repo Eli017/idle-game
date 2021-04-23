7639e0220dc1f24ac1525fbd0166eac7e144f5c7
Eli Sokeland <etsokeland@bsu.edu>
2021-04-23 14:32:02 -0400

    Added Time Loop

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
 import logo from './logo.svg';
+import {useState, useEffect} from "react";
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [timeout, setTimeout] = useState(1000);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      onUpdate();
+    }, timeout);
+    return () => clearInterval(interval);
+  });
+
+
+  //What happens within each second of the game?
+  const onUpdate = () => {
+    setCount(count + 1);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {count}
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          onClick={() => setTimeout(100)}
         >
-          Learn React
+          Increase time speed
         </a>
       </header>
     </div>
